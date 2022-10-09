bc6f494639d4c398fbd4b5713a2961690aa2b037
Achref <medachrefg@gmail.com>
2022-10-09 09:06:28 +0100

    first commit

---
+++ comments/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors=require('cors')
+const { randomBytes } = require('crypto');
+const app = express();
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+app.use(cors())
+
+const commentsByPostId = {};
+
+app.post('/posts/:id/comments', (req, res) => {
+	const commentId = randomBytes(4).toString('hex');
+
+	const { content } = req.body;
+
+	const comments = commentsByPostId[req.params.id] || [];
+
+	comments.push({ id: commentId, content });
+	commentsByPostId[req.params.id] = comments;
+
+	res.status(201).send(comments);
+});
+
+app.get('/posts/:id/comments', (req, res) => {
+	res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.listen(4001, () => {
+	console.log('server of the comments run on 4001');
+});
---
+++ posts/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors=require('cors')
+const morgan = require('morgan');
+const { randomBytes } = require('crypto');
+const app = express();
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+app.use(cors())
+const posts = {};
+app.get('/posts', (req, res) => {
+	res.send(posts);
+});
+
+app.post('/posts', (req, res) => {
+	const id = randomBytes(4).toString('hex');
+	const { title } = req.body;
+	posts[id] = { id, title };
+
+	res.status(201).send(posts[id]);
+	console.log(posts[id]);
+});
+
+app.listen(4000, () => {
+	console.log('listening on port 4000');
+});
